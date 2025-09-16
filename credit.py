7c69b577f2d98eddb08d88a03c8af13b17746ece
A Vidhan Reddy <itsvidhanreddy@gmail.com>
2025-09-16 06:51:26 +0000

    Refactor credit card masking functionality into a class structure

--- credit.py
+++ credit.py
@@ -1,19 +1,31 @@
-import re
+import re as regex
 
+class CardMasking():
+   cardNumber = ''
+   maskedCardNumber = ''
 
-def mask_cc_number(cc_string, digits_to_keep=4, mask_char='*'):
-   cc_string_total = sum(map(str.isdigit, cc_string))
+   # constructor
+   def __init__(self, cardNumber):
+      self.cardNumber = cardNumber
 
-   if len(Credit_Card) != 16:
-      print("Invalid Credit Card number.")
-   if len(Credit_Card) == 16:
-      digits_to_mask = cc_string_total - digits_to_keep
-      masked_cc_string = re.sub('\d', mask_char, cc_string, digits_to_mask)
+   # method
+   def maskCreditOrDebitCardDetails(
+         self,
+         digitsToKeep: int = 4,
+         maskCharacter = '*'
+   ):
+      cardNumberTotal = sum(map(str.isdigit, self.cardNumber))
+      cardNumberLength = len(self.cardNumber)
 
-      return masked_cc_string
+      if (cardNumberLength != 16):
+         print("Invalid Credit/Debit Card Number.")
 
-Credit_Card=input("Enter any Credit Card number: ")
+      if (cardNumberLength == 16):
+         digitsToMask = cardNumberTotal - digitsToKeep
+         self.maskedCardNumber = regex.sub(r'\d', maskCharacter, self.cardNumber, digitsToMask)
+         
+      return self.maskedCardNumber    
 
-print(mask_cc_number(Credit_Card))
-
-
+# init
+newCard = CardMasking(input("Enter you Credit/Debit Card Number: "))
+print(newCard.maskCreditOrDebitCardDetails())
